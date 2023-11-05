34741526244655bf5a4d89a695ba3498f7a05d19
thang <mthang3i@gmail.com>
2023-11-05 23:52:14 +0700

    the first commit

---
+++ lib/utils/localization/language/language_vn.dart
@@ -0,0 +1,502 @@
+import 'package:mealup/utils/localization/language/languages.dart';
+
+class LanguageVn extends Languages {
+  @override
+  String get screenSetting=>'Cài đặt';
+  @override
+  String get walletSetting=>'Ví';
+  @override
+  String get labelSelectLanguage=>'Chọn ngôn ngữ';
+  @override
+  String get labelEditPersonalInfo=>'Chỉnh sửa thông tin cá nhân';
+  @override
+  String get labelSavePersonalInfo=>'Lưu thông tin cá nhân';
+  @override
+  String get labelManageYourLocation=>'Quản lý vị trí của bạn';
+  @override
+  String get labelChangePassword=>'Đổi mật khẩu';
+  @override
+  String get labelLanguage=>'Ngôn ngữ';
+  @override
+  String get labelAboutApp=>'Giới thiệu về ứng dụng';
+  @override
+  String get labelPrivacyPolicy=>'Chính sách bảo mật';
+  @override
+  String get labelTermOfUse=>'Điều khoản sử dụng';
+  @override
+  String get labelFeedbacknSup=>'Phản hồi & Hỗ trợ';
+  @override
+  String get labelFAQs=>'Câu hỏi thường gặp';
+  @override
+  String get labelRemoveAccount=>'Xóa tài khoản của bạn';
+  @override
+  String get labelLogout=>'Đăng xuất';
+  @override
+  String get labelMealupAppVersion=>'Phiên bản ứng dụng';
+  @override
+  String get labelScreenIntro1Line1=>'Khám phá nhà hàng gần bạn';
+  @override
+  String get labelScreenIntro1Line2=>'Giúp việc tìm đồ ăn trở nên đơn giản, bạn chỉ cần nhập địa chỉ và làm phần còn lại.';
+  @override
+  String get labelSkip=>'NHẢY';
+  @override
+  String get labelScreenIntro2Line1=>'Nhà hàng giao hàng tận nhà ngay lập tức';
+  @override
+  String get labelScreenIntro2Line2=>'Dịch vụ giao hàng tận nơi trong thời gian của bạn với chất lượng thực phẩm tốt nhất.';
+  @override
+  String get labelScreenIntro3Line1=>'Theo dõi bữa ăn của bạn';
+  @override
+  String get labelScreenIntro3Line2=>'Đặt bữa ăn yêu thích của bạn từ một nhà hàng và xem bữa ăn của bạn cách bạn bao xa bằng tính năng theo dõi trực tiếp.';
+  @override
+  String get labelEmail=>'E-mail';
+  @override
+  String get labelPassword=>'Mật khẩu';
+  @override
+  String get labelForgotPassword=>'Quên mật khẩu ?';
+  @override
+  String get labelForgotPassword1=>'Quên mật khẩu';
+  @override
+  String get labelEnterYourPassword=>'Nhập mật khẩu của bạn';
+  @override
+  String get labelEnterYourEmailID=>'Nhập email của bạn';
+  @override
+  String get labelRememberMe=>'Nhớ tôi';
+  @override
+  String get labelLogin=>'Đăng nhập';
+  @override
+  String get labelDonthaveAcc=>'Bạn chưa có tài khoản? "';
+  @override
+  String get labelCreateNow=>'Đăng ký';
+  @override
+  String get labelExploreAppwithoutLogin=>'Khám phá ứng dụng mà không cần đăng nhập';
+  @override
+  String get labelSkipNow=>'Bỏ qua ngay';
+  @override
+  String get labelCreateNewAccount=>'Tạo tài khoản mới';
+  @override
+  String get labelFullName=>'Họ và tên';
+  @override
+  String get labelEnterYourFullName=>'Nhập tên đầy đủ của bạn.';
+  @override
+  String get labelContactNumber=>'Số liên lạc';
+  @override
+  String get labelConfirmPassword=>'Xác nhận mật khẩu';
+  @override
+  String get labelAlreadyHaveAccount=>'Bạn co săn san để tạo một tai khoản ?';
+  @override
+  String get labelReEnterPassword=>'Nhập lại mật khẩu của bạn';
+  @override
+  String get labelOTP=>'OTP';
+  @override
+  String get labelEnterOTP=>'Nhập OTP';
+  @override
+  String get labelVerifyNow=>'Xác minh ngay bây giờ';
+  @override
+  String get labelDontReceiveCode=>'Bạn không nhận được mã? "';
+  @override
+  String get labelResendAgain=>'Gửi lại lần nữa';
+  @override
+  String get labelOTPBottomLine=>'Chúng tôi sẽ chia sẻ một mã OTP, Không chia sẻ OTP của bạn với bất kỳ người nào khác, Vui lòng kiểm tra thiết bị của bạn."';
+  @override
+  String get labelSubmitThis=>'Nộp';
+  @override
+  String get labelChangePasswordBottomline=>'Kiểm tra ID email của bạn, chúng tôi sẽ chia sẻ một OTP với bạn. Đừng chia sẻ OTP của bạn với bất kỳ ai khác."';
+  @override
+  String get labelNewPassword=>'mật khẩu mới';
+  @override
+  String get labelEnterNewPassword=>'Nhập mật khẩu mới';
+  @override
+  String get labelReEnterNewPassword=>'Nhập lại mật khẩu mới';
+  @override
+  String get labelPasswordRequired=>'Mật khẩu là bắt buộc';
+  @override
+  String get labelPasswordConfPassNotMatch=>'Mật khẩu và Xác nhận mật khẩu không khớp.';
+  @override
+  String get labelPasswordValidation=>'Mật khẩu bắt buộc: Bảng chữ cái, số & 8 ký tự';
+  @override
+  String get labelProfile=>'Hồ sơ';
+  @override
+  String get labelYourFavorites=>'Yêu thích của bạn';
+  @override
+  String get labelOrderHistory=>'Lịch sử đơn hàng';
+  @override
+  String get labelShareWithFriends=>'Chia sẻ với bạn bè';
+  @override
+  String get labelCart=>'Giỏ hàng';
+  @override
+  String get labelExplore=>'Khám phá';
+  @override
+  String get labelHome=>'Trang chủ';
+  @override
+  String get labelExploreRest=>'Khám phá nhà hàng';
+  @override
+  String get labelExploreTheBestCuisines=>'Khám phá những món ăn ngon nhất';
+  @override
+  String get labelNonPureVegRest=>'Nhà hàng không ăn chay';
+  @override
+  String get labelPureVegRest=>'Nhà hàng chay thuần túy';
+  @override
+  String get labelTopRestaurantsNear=>'Nhà hàng hàng đầu gần bạn';
+  @override
+  String get labelTopRest=>'Nhà hàng hàng đầu';
+  @override
+  String get labelSelectAddress=>'Chọn địa chỉ';
+  @override
+  String get labelNoRestNear=>'Không tìm thấy nhà hàng nào gần bạn.';
+  @override
+  String get labelNoData=>'Không có dữ liệu.';
+  @override
+  String get labelPleaseWait=>'Vui lòng chờ...';
+  @override
+  String get labelYourCart=>'Giỏ hàng của bạn';
+  @override
+  String get labelBookOrder=>'Đặt hàng';
+  @override
+  String get labelDelivery=>'Vận chuyển';
+  @override
+  String get labelTakeaway=>'Mua mang về';
+  @override
+  String get labelAddMoreItems=>'Thêm các mục khác';
+  @override
+  String get labelCustomizable=>'Có thể tùy chỉnh';
+  @override
+  String get labelAddRequestToRest=>'Bạn có yêu cầu gì về món ăn ngon không?';
+  @override
+  String get labelOptional=>'Không bắt buộc';
+  @override
+  String get labelFoodOfferCoupons=>'Phiếu giảm giá ưu đãi thực phẩm';
+  @override
+  String get labelSearchRestOrCoupon=>'Tìm kiếm nhà hàng hoặc mã giảm giá...';
+  @override
+  String get labelYouHaveCoupon=>'Bạn có một phiếu giảm giá để sử dụng';
+  @override
+  String get labelApplyIt=>'Áp dụng nó';
+  @override
+  String get labelSubtotal=>'Tổng phụ';
+  @override
+  String get labelAppliedCoupon=>'Phiếu giảm giá áp dụng';
+  @override
+  String get labelRemoveCoupon=>'Xóa phiếu giảm giá';
+  @override
+  String get labelDeliveryCharge=>'Phí giao hàng';
+  @override
+  String get labelTax=>'Thuế';
+  @override
+  String get labelVendorDiscount=>'Giảm giá của nhà cung cấp';
+  @override
+  String get labelGrandTotal=>'Tổng cộng';
+  @override
+  String get labelSetLocation=>'Thiết lập địa điểm';
+  @override
+  String get labelSearchSomething=>'Tìm kiếm thứ gì đó...';
+  @override
+  String get labelRecentlySearches=>'Tìm kiếm gần đây';
+  @override
+  String get labelClear=>'Thông thoáng';
+  @override
+  String get labelSearchByFood=>'Tìm kiếm theo món ăn';
+  @override
+  String get labelSearchByTopBrands=>'Tìm kiếm theo thương hiệu hàng đầu';
+  @override
+  String get labelCancel=>'Hủy bỏ';
+  @override
+  String get labelApplyFilter=>'Áp dụng bộ lọc';
+  @override
+  String get labelFilter=>'Lọc';
+  @override
+  String get labelSortingBy=>'Sắp xếp theo (Xếp hạng)';
+  @override
+  String get labelQuickFilters=>'Bộ lọc nhanh';
+  @override
+  String get labelCousines=>'anh em họ';
+  @override
+  String get labelAddNewAddress=>'Thêm địa chỉ mới';
+  @override
+  String get labelSavedAddress=>'Địa chỉ đã lưu';
+  @override
+  String get labelPleaseAddAddress=>'Vui lòng thêm địa chỉ.';
+  @override
+  String get labelEditAddress=>'sửa địa chỉ';
+  @override
+  String get labelRemoveThisAddress=>'Xóa địa chỉ này';
+  @override
+  String get labelContinue=>'Tiếp tục';
+  @override
+  String get labelAddAddress=>'Thêm địa chỉ';
+  @override
+  String get labelRemoveAddress=>'Xóa địa chỉ';
+  @override
+  String get labelNoGoBack=>'Không, quay lại';
+  @override
+  String get labelYesRemoveIt=>'Có, hãy xóa nó';
+  @override
+  String get labelSearchLocation=>'Tìm kiếm vị trí';
+  @override
+  String get labelHouseNo=>'Số nhà/Số căn hộ/Tầng/Tòa nhà';
+  @override
+  String get labelTypeFullAddressHere=>'Nhập địa chỉ đầy đủ vào đây';
+  @override
+  String get labelLandmark=>'Phong cảnh';
+  @override
+  String get labelAnyLandmarkNearYourLocation=>'Bất kỳ địa danh nào gần vị trí của bạn';
+  @override
+  String get labelAttachLabel=>'Đính kèm nhãn';
+  @override
+  String get labelAddLabelForThisLocation=>'Thêm nhãn cho vị trí này';
+  @override
+  String get labelPleaseSearchAddress=>'Vui lòng tìm kiếm địa chỉ của bạn để thêm địa chỉ của bạn.';
+  @override
+  String get labelPleaseAddLabelForAddress=>'Vui lòng nhập nhãn cho địa chỉ.';
+  @override
+  String get labelSaveIt=>'Lưu nó';
+  @override
+  String get labelUpdate=>'Cập nhật';
+  @override
+  String get labelVersion=>'Phiên bản';
+  @override
+  String get labelAddYourExperience=>'Thêm trải nghiệm của bạn';
+  @override
+  String get labelAddYourExperienceHere=>'Thêm kinh nghiệm của bạn ở đây';
+  @override
+  String get labelMax3Image=>'Tối đa 3 hình ảnh cho phép tải lên.';
+  @override
+  String get labelPleaseSelectEmoji=>'Vui lòng đưa ra tỷ lệ từ các biểu tượng cảm xúc ở trên.';
+  @override
+  String get labelShareFeedback=>'Chia sẻ phản hồi';
+  @override
+  String get labelPhotoLibrary=>'Thư viện ảnh';
+  @override
+  String get labelCamera=>'Máy ảnh';
+  @override
+  String get labelPaymentMethod=>'Phương thức thanh toán';
+  @override
+  String get labelCashOnDelivery=>'thanh toán khi giao hàng';
+  @override
+  String get labelPleaseSelectPaymentMethod=>'Vui lòng chọn phương thức thanh toán.';
+  @override
+  String get labelPlaceYourOrder=>'Đặt hàng của bạn';
+  @override
+  String get labelClearList=>'Danh mục rõ ràng';
+  @override
+  String get labelOrderedOn=>'Đặt hàng vào';
+  @override
+  String get labelCanceledOn=>'Đã hủy vào';
+  @override
+  String get labelAcceptedOn=>'Đã chấp nhận vào';
+  @override
+  String get labelApproveOn=>'Đồng ý';
+  @override
+  String get labelDeliveredOn=>'Đã giao vào ngày';
+  @override
+  String get labelPickedUpOn=>'Đã nhặt được trên';
+  @override
+  String get labelRejectedOn=>'Bị từ chối vào';
+  @override
+  String get labelOrderPending=>'Đơn hàng đang chờ xử lý.';
+  @override
+  String get labelOrderCanceled=>'Lệnh hủy.';
+  @override
+  String get labelOrderAccepted=>'Đơn hàng được chấp nhận.';
+  @override
+  String get labelOrderCompleted=>'Đơn hàng đã hoàn tất.';
+  @override
+  String get labelOrderPickedUp=>'Đơn hàng đã được nhận.';
+  @override
+  String get labelOrderRejected=>'Đơn đặt hàng bị từ chối.';
+  @override
+  String get labelDeliveredSuccess=>'Đã giao hàng thành công.';
+  @override
+  String get labelRateNow=>'Bình chọn bây giờ';
+  @override
+  String get labelTrackOrder=>'Theo dõi thứ tự';
+  @override
+  String get labelOrderDetails=>'Chi tiết đặt hàng';
+  @override
+  String get labelCancelOrder=>'Hủy đơn hàng';
+  @override
+  String get labelDeliveredBy=>'Được giao bởi';
+  @override
+  String get labelViewOrderDetails=>'Xem chi tiết đơn hàng';
+  @override
+  String get labelFoodBeingPrepared=>'Thức ăn đang được chuẩn bị.';
+  @override
+  String get labelFoodReadyPickup=>'Thức ăn đã sẵn sàng để đón';
+  @override
+  String get labelOrderReview=>'Đánh giá đơn hàng';
+  @override
+  String get labelGiveStar=>'tặng sao';
+  @override
+  String get labelReviewThisFood=>'Đánh giá món ăn này';
+  @override
+  String get labelAddYourValuableFeedback=>'Thêm phản hồi có giá trị của bạn';
+  @override
+  String get labelPleaseGiveStar=>'Xin vui lòng cho ngôi sao đánh giá.';
+  @override
+  String get labelSubmitIt=>'Nộp';
+  @override
+  String get labelInternalServerError=>'Lỗi máy chủ nội bộ.';
+  @override
+  String get labelEmailRequired=>'Email thì cần thiết.';
+  @override
+  String get labelEnterValidEmail=>'Nhập email hợp lệ';
+  @override
+  String get labelFeedbackCommentRequired=>'Bình luận phản hồi là bắt buộc.';
+  @override
+  String get labelContactNumberRequired=>'Số liên lạc là bắt buộc.';
+  @override
+  String get labelContactNumberNotValid=>'Số liên lạc không hợp lệ.';
+  @override
+  String get labelFullNameRequired=>'Tên đầy đủ là bắt buộc.';
+  @override
+  String get labelEmailPasswordWrong=>'Email hoặc mật khẩu sai..!!';
+  @override
+  String get labelEmailIdAlreadyTaken=>'Id email đã được sử dụng.';
+  @override
+  String get labelInvalidData=>'Dữ liệu không hợp lệ';
+  @override
+  String get labelFactAQuestions=>'Thực tế một câu hỏi';
+  @override
+  String get labelErrorWhileUpdate=>'Lỗi trong khi cập nhật.';
+  @override
+  String get labelTotalItem=>'Tổng số mặt hàng';
+  @override
+  String get labelSearchItems=>'Tìm kiếm các mục';
+  @override
+  String get labelRatings=>'Xếp hạng';
+  @override
+  String get labelFor2Persons=>'Dành cho 2 người';
+  @override
+  String get labelItem=>'Mục';
+  @override
+  String get labelRemoveCartItem=>'Xóa mặt hàng trong giỏ hàng?';
+  @override
+  String get labelYourCartContainsDishesFrom=>'Giỏ hàng của bạn chứa các món ăn từ';
+  @override
+  String get labelYourCartContains1=>'Bạn có muốn hủy lựa chọn và thêm các món ăn từ';
+  @override
+  String get labelAreYouSureToRemove=>'Bạn có chắc chắn muốn xóa nhà hàng này không?';
+  @override
+  String get labelRemoveFromTheList=>'Xóa khỏi danh sách';
+  @override
+  String get labelValidUpTo=>'Có hiệu lực đến';
+  @override
+  String get labelHighToLow=>'Cao đến thấp';
+  @override
+  String get labelLowToHigh=>'Thấp đến cao';
+  @override
+  String get labelVegRestaurant=>'Ăn chay. Nhà hàng';
+  @override
+  String get labelNonVegRestaurant=>'Không ăn chay. Nhà hàng';
+  @override
+  String get labelBothVegNonVeg=>'Cả hai đều ăn chay. & Không ăn chay.';
+  @override
+  String get labelPleaseLoginToAddFavorite=>'Vui lòng đăng nhập để thêm vào mục yêu thích';
+  @override
+  String get labelKmFarAway=>'km xa';
+  @override
+  String get labelDeliveryUnavailable=>'Rất tiếc, hiện tại việc giao hàng không có sẵn ..!';
+  @override
+  String get labelTakeawayUnavailable=>'Xin lỗi, Hiện tại Takeaway không có sẵn..!';
+  @override
+  String get labelNoOrderHistory=>'Bạn chưa có lịch sử đặt hàng nào"';
+  @override
+  String get labelNoOffer=>'Ưu đãi giảm giá hiện không có sẵn cho nhà hàng này.';
+  @override
+  String get labelAboutCompany=>'Giới thiệu về công ty';
+  @override
+  String get labelPREPAREFORORDER=>'Chuẩn bị đặt hàng';
+  @override
+  String get labelREADYFORORDER=>'Sẵn sàng cho đơn hàng';
+  @override
+  String get labelOrderCancelReason=>'Lý do hủy đơn hàng';
+  @override
+  String get labelTypeOrderCancelReason=>'Nhập đơn hàng Lý do hủy';
+  @override
+  String get labelYesCancelIt=>'Có, Hủy nó';
+  @override
+  String get labelPleaseEnterCancelReason=>'Vui lòng nhập lý do hủy đơn hàng.';
+  @override
+  String get labelRaiseRefundRequest=>'Tăng yêu cầu hoàn tiền';
+  @override
+  String get labelRaiseRefundRequestReason=>'Lý do yêu cầu hoàn tiền';
+  @override
+  String get labelRaiseRefundRequestReason1=>'Nhập lý do yêu cầu hoàn tiền';
+  @override
+  String get labelYesRaiseIt=>'Vâng, nâng nó lên';
+  @override
+  String get labelPleaseEnterRaiseRefundReq=>'Vui lòng nhập lý do yêu cầu hoàn tiền';
+  @override
+  String get iFSCCode=>'Mã IFSC';
+  @override
+  String get iFSCCode1=>'Nhập mã IFSC';
+  @override
+  String get mICRCode=>'Mã MICR';
+  @override
+  String get mICRCode1=>'Nhập mã MICR';
+  @override
+  String get bankAccountName=>'Tên tài khoản ngân hàng';
+  @override
+  String get bankAccountName1=>'Nhập tên tài khoản ngân hàng';
+  @override
+  String get bankAccountNumber=>'Số tài khoản ngân hàng';
+  @override
+  String get bankAccountNumber1=>'Nhập số tài khoản ngân hàng';
+  @override
+  String get labelSubmit=>'Nộp';
+  @override
+  String get iFSCCode2=>'MÃ IFSC là bắt buộc.';
+  @override
+  String get mICRCode2=>'MÃ MICR là bắt buộc.';
+  @override
+  String get bankAccountName2=>'Tên tài khoản ngân hàng là bắt buộc.';
+  @override
+  String get bankAccountNumber2=>'Số tài khoản ngân hàng là bắt buộc.';
+  @override
+  String get labelFinancialDetails=>'Chi tiết tài chính';
+  @override
+  String get labelPersonalDetails=>'Thông tin cá nhân';
+  @override
+  String get labelLoginSuccessfully=>'Đăng nhập thành công';
+  @override
+  String get labelAreYouSureExit=>'Bạn có chắc bạn muốn thoát?';
+  @override
+  String get labelConfirmExit=>'Xác nhận Thoát';
+  @override
+  String get labelNO=>'KHÔNG';
+  @override
+  String get labelYES=>'ĐÚNG';
+  @override
+  String get labelErrorWhileAddAddress=>'Lỗi khi thêm địa chỉ';
+  @override
+  String get walletBalance=>'Số dư trên Ví';
+  @override
+  String get transactionHistory=>'Lịch sử giao dịch';
+  @override
+  String get topUpWallet=>'VÍ NẠP TIỀN';
+  @override
+  String get addMoney=>'Thêm tiền';
+  @override
+  String get availableMealUpBalance=>'Số dư MealUp có sẵn';
+  @override
+  String get enterAmount=>'Nhập số tiền';
+  @override
+  String get mealUpWallet=>'Ví MealUp';
+  @override
+  String get moneyWillBeAddedToMealUpWallet=>'Tiền sẽ được thêm vào ví MealUp';
+  @override
+  String get labelAreYouSureLogout=>'Bạn có chắc chắn bạn muốn thoát ?';
+  @override
+  String get labelConfirmLogout=>'Xác nhận đăng xuất';
+  @override
+  String get labelEnterOldPassword=>'Nhập mật khẩu cũ';
+  @override
+  String get labelOldPassword=>'Mật khẩu cũ';
+  @override
+  String get thisIsATestingApp=>'Đây là một ứng dụng thử nghiệm';
+  @override
+  String get changeProfilePicture=>'Thay đổi hình ảnh đại diện';
+  @override
+  String get noCustomizationAvailable=>'Không có sẵn dữ liệu tùy chỉnh.';
+  @override
+  String get outOfStock=>'Hết hàng';
+}
